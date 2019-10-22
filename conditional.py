b01439fe6e200cd0c5a6d3ab0e8135001f7d0358
jpaltahona <jpaltahonacc@gmail.com>
2019-10-21 20:28:47 -0500

    add condicionales

---
+++ conditional.py
@@ -0,0 +1,15 @@
+first_name ="jua"
+last_nae = "pierr"
+
+if first_name == "jean":
+    if last_nae == "pierre":
+        print('you are jean pierre')
+    else:
+        print('you are not jean pierre')
+else:
+    print('tu no eres nadie')
+    
+x = 2
+
+if x > 3 and x < 2:
+    print('x es mayor que 3 y menor que 2')
